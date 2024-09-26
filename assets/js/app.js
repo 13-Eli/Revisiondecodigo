f7f720ae364291a934f359ff77f6b67b25716ca3
13-Elí <elidiazr13@gmail.com>
2024-09-26 12:24:51 -0600

    se corrigieron errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,23 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try{
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); //Obtener Json
+    console.log(data);
+    $n.textContent = `${data.name}`; // modificar ' por `
+    $b.textContent = `${data.blog}`; // modificar ' por `
+    $l.textContent = `${data.location}`; // modificar ' por `
+  } catch (err) {
+ 
+  function handleError(err) {
+    console.log('OH NO!');
+    console.log(err);
+    n.textContent = `Algo salió mal: ${err}`
+  }
+  
+  displayUser('stolinski').catch(handleError);
 }
-
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
 }
-
-displayUser('stolinski').catch(handleError);
